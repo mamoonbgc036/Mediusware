01a11fbbcb6e1eac51dbd41b7c04ff8facefe162
Abdullah Al Mamoon <mamoon036@gmail.com>
2022-12-22 21:15:54 +0600

    22.12.2022

---
+++ resources/js/components/Good.vue
@@ -0,0 +1,51 @@
+<template>
+     <div class="card-body">
+            <div class="table-response">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Variant</th>
+                        <th width="150px">Action</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+
+                    <tr>
+                        <td>1</td>
+                        <td>T-Shirt <br> Created at : 25-Aug-2020</td>
+                        <td>Quality product in low cost and comfortable.</td>
+                        <td>
+                            <dl class="row mb-0" style="height: 80px; overflow: hidden" id="variant">
+
+                                <dt class="col-sm-3 pb-0">
+                                    SM/ Red/ V-Nick
+                                </dt>
+                                <dd class="col-sm-9">
+                                    <dl class="row mb-0">
+                                        <dt class="col-sm-4 pb-0">Price : 200 </dt>
+                                        <dd class="col-sm-8 pb-0">InStock : </dd>
+                                    </dl>
+                                </dd>
+                            </dl>
+                            <button onclick="$('#variant').toggleClass('h-auto')" class="btn btn-sm btn-link">Show more</button>
+                        </td>
+                        <td>
+                            <div class="btn-group btn-group-sm">
+                                <a href="" class="btn btn-success">Edit</a>
+                            </div>
+                        </td>
+                        
+                    </tr>
+                    </tbody>
+
+                </table>
+            </div>
+
+        </div>
+</template>
+<script>
+</script>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Products</h1>
+    </div>
+
+
+    <div class="card">
+        <form action="" method="get" class="card-header">
+            <div class="form-row justify-content-between">
+                <div class="col-md-2">
+                    <input type="text" name="title" placeholder="Product Title" class="form-control">
+                </div>
+                <div class="col-md-2">
+                    <select name="variant" id="" class="form-control">
+
+                    </select>
+                </div>
+
+                <div class="col-md-3">
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text">Price Range</span>
+                        </div>
+                        <input type="text" name="price_from" aria-label="First name" placeholder="From" class="form-control">
+                        <input type="text" name="price_to" aria-label="Last name" placeholder="To" class="form-control">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <input type="date" name="date" placeholder="Date" class="form-control">
+                </div>
+                <div class="col-md-1">
+                    <button type="submit" class="btn btn-primary float-right"><i class="fa fa-search"></i></button>
+                </div>
+            </div>
+        </form>
+
+        <div id="app">
+            <products></products>
+        </div>
+
+        <div class="card-footer">
+            <div class="row justify-content-between">
+                <div class="col-md-6">
+                    <p>Showing 1 to 10 out of 100</p>
+                </div>
+                <div class="col-md-2">
+
+                </div>
+                <nav aria-label="Page navigation example">
+  <ul class="pagination">
+    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+  </ul>
+</nav>
+            </div>
+        </div>
+    </div>
+
+@endsection
